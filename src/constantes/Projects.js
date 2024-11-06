ca38f552f99b2814065aab1932e5ac9d2243dddc
eljabriyassine <yassinejabrio.2001@gmail.com>
2024-11-06 13:44:56 +0100

    update project

--- src/constantes/Projects.js
+++ src/constantes/Projects.js
@@ -1,12 +1,5 @@
 export const Projects = [
   {
-    id: 0,
-    title: 'Portflolio ',
-    img: 'https://img.freepik.com/free-vector/dark-blue-memphis-blog-banner-template_53876-98946.jpg',
-    desc: 'A portfolio showcases my best work, skills, and achievements, demonstrating my expertise and capabilities ',
-    link: 'https://github.com/eljabriyassine/mtPortfolio',
-  },
-  {
     id: 1,
     title: 'Music app',
     img: 'https://images.pexels.com/photos/1128440/pexels-photo-1128440.jpeg?auto=compress&cs=tinysrgb&w=400',
@@ -22,9 +15,24 @@ export const Projects = [
   },
   {
     id: 3,
-    title: 'To Do list',
-    img: 'https://images.pexels.com/photos/5717409/pexels-photo-5717409.jpeg?auto=compress&cs=tinysrgb&w=400',
-    desc: 'A task management tool with the ability to organize and prioritize tasks.',
-    link: 'https://github.com/eljabriyassine/todo_list',
+    title: 'Youtube Clone',
+    img: 'https://miro.medium.com/v2/resize:fit:1400/1*rQB4fmakQa_Cwhwzgsy9YQ.png',
+    desc: `Le projet permet aux utilisateurs de télécharger, gérer et modifier des vidéos avec titres, descriptions, miniatures et tags. Il prend en charge la recherche de vidéos, les likes, les abonnements et le mode sombre, avec un stockage cloud pour les médias`,
+    link: 'https://github.com/eljabriyassine/youtube-clone-with-nextjs',
+  },
+
+  {
+    id: 4,
+    title: 'chat app ',
+    img: 'https://github.com/eljabriyassine/chat-app-with-socket/raw/main/frontend/public/screenshot-for-readme.png',
+    desc: `Le projet offre un chat en temps réel avec gestion de statut en ligne, messagerie instantanée et gestion d'erreurs, tout en permettant l'authentification et l'autorisation sécurisée des utilisateurs`,
+    link: 'https://github.com/eljabriyassine/chat-app-with-socket',
+  },
+  {
+    id: 5,
+    title: 'E-commerce store',
+    img: 'https://github.com/burakorkmez/mern-ecommerce/raw/master/frontend/public/screenshot-for-readme.png',
+    desc: `Le projet e-commerce gère l'inscription des utilisateurs, le catalogue de produits, les paniers d'achat, le paiement sécurisé, les codes de réduction et un tableau de bord administrateur pour suivre les ventes.`,
+    link: 'https://github.com/eljabriyassine/ecommerce-store',
   },
 ];
